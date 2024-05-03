9f77d6510a2966ce7fcdd6dc50f4759f580fb216
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-04 02:56:08 +0900

    Auth and Jwt  Dto
    Auth and Jwt  DTO creation

---
+++ ContactService/src/main/java/com/loollablk/contactservice/entityDto/JwtResponseDTO.java
@@ -0,0 +1,15 @@
+package com.loollablk.contactservice.entityDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class JwtResponseDTO {
+    private String accessToken;
+
+}
